071a5a4dc7dfbf2cb43fc68d61f17edce25e39fc
Phil-Bastian Berndt <me@pbb.io>
2020-10-22 13:13:58 +0200

    Update test-command.sh

--- test/test-command.sh
+++ test/test-command.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-
 set -e 
 
-# Clean the app folder
+# clean the app folder
 cd /opt/
 rm -rf app
 
-# Create laravel project into app
+# create laravel project into app
 composer create-project --prefer-dist laravel/laravel app
 
-# Start services in the background.
+# start services in the background
 runsvdir /etc/service &
 
-# Check if the Services are available
+# check if the services are available
 wait-for-it.sh --host=localhost --port=8080
